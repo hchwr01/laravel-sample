cd9b381bb329ae51973eb186db6b6e449c9afa60
hchwr <103794804+hchwr01@users.noreply.github.com>
2026-06-17 08:28:15 +0900

    core: vite
    設定見直し

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/laravel/',
+    base: '/laravel/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
